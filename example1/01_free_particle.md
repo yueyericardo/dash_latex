3a6a462a504df5666085a2cbed03fc074a5063e8
richard <yueyericardo@gmail.com>
2020-04-16 10:29:47 -0400

    example1

---
+++ example1/01_free_particle.md
@@ -0,0 +1,74 @@
+# Free Particle
+
+An important question to ask is: what is the probability to find a free particle within a $\Delta x$ region in space? 
+
+This is crucial for measurements, since we would never measure ALL space at the same time, but instead, our instrument will measure a section of $x$ space 
+
+##### 1. Some equation `works`
+
+$$\hat H\left|k\right> = -\frac{\hbar^2}{2m} \frac{\partial^2\left|k\right>}{\partial x^2}
+= -\frac{\hbar^2}{2m} \frac{\partial^2}{\partial x^2}\left(\frac{1}{\sqrt{2\pi}}e^{ikx} \right)$$
+
+$$= \frac{\hbar^2k^2}{2m} \left(\frac{1}{\sqrt{2\pi}}e^{ikx} \right)  = \frac{p^2}{2m}\left|k\right>$$
+
+The probability of finding it within a $\Delta x$, at **any** place in space, is the same.
+
+*Thus when the particle has a well-defined momentum ($\hbar k$),  the probability of finding it anywhere in space is the same.*
+
+
+***Localization*** of the particle can be obtained by considering not just a single value of momentum ($\hbar k$), but a range of values.  
+Consider the superposition of waves with a range of momenta given by 
+$p=p_0 \pm \Delta p$ $\hspace{0.25cm}$ or $\hspace{0.25cm}$ $\hbar k = \hbar(k_0 \pm \Delta k)$
+
+
+Since the momentum varies continuolsy, we use an integration for the superposition of waves, all equally weigthed:   
+
+$$\Psi_{\Delta k}(x) = \frac{2N\sin(\Delta kx)}{x} e^{ik_ox}$$
+
+where $N$ is a normalization constant and is equal to $N=\frac{1}{2\sqrt{\pi\Delta k}}$
+
+
+To understand this new wavefunction, we plot its Real and Imaginary components separately.   
+
+
+##### 2. Some equation `not works`
+
+###### Test 1
+
+`input`
+
+```
+$$\left[\hat P, \hat H \right] = \hat P \cdot \hat H - \hat H \cdot \hat P$$ 
+```
+
+`output` from dcc.Markdown, accidently put `<a>` tag into equation, so mathjax cannot render it
+
+```
+$$\left<a href="">\hat P, \hat H \right</a> = \hat P \cdot \hat H - \hat H \cdot \hat P$$
+```
+
+`rendered` (not work)
+
+$$\left[\hat P, \hat H \right] = \hat P \cdot \hat H - \hat H \cdot \hat P$$ 
+
+###### Test 2
+
+`input`
+
+```
+$$\Psi^*_k(x) \Psi_k(x) \Delta x = \left|\Psi_k(x)\right|^2 \Delta x 
+= \left(\frac{1}{\sqrt{2\pi}} e^{ikx}\right)^*\frac{1}{\sqrt{2\pi}} e^{ikx} \Delta x = \frac{1}{2\pi} \Delta x $$
+```
+
+`output` from dcc.Markdown, accidently put `<em>` tag into equation, so mathjax cannot render it
+
+```
+$$\Psi^<em>_k(x) \Psi_k(x) \Delta x = \left|\Psi_k(x)\right|^2 \Delta x = \left(\frac{1}{\sqrt{2\pi}} e^{ikx}\right)^</em>\frac{1}{\sqrt{2\pi}} e^{ikx} \Delta x = \frac{1}{2\pi} \Delta x $$
+```
+
+`rendered` (not work)
+
+$$\Psi^*_k(x) \Psi_k(x) \Delta x = \left|\Psi_k(x)\right|^2 \Delta x = \left(\frac{1}{\sqrt{2\pi}} e^{ikx}\right)^*\frac{1}{\sqrt{2\pi}} e^{ikx} \Delta x = \frac{1}{2\pi} \Delta x $$
+
+
+##### 3. Graph axis works
---
+++ example1/free_particle.py
@@ -0,0 +1,146 @@
+import os
+import dash
+import numpy as np
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+###### important for latex ######
+import dash_defer_js_import as dji
+
+filepath = os.path.split(os.path.realpath(__file__))[0]
+
+external_stylesheets = ['https://codepen.io/yueyericardo/pen/OJyLrKR.css',
+                        'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/styles/monokai-sublime.min.css']
+
+
+app = dash.Dash(name='free_particle',
+                external_stylesheets=external_stylesheets)
+
+###### important for latex ######
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            <script type="text/x-mathjax-config">
+            MathJax.Hub.Config({
+                tex2jax: {
+                inlineMath: [ ['$','$'],],
+                processEscapes: true
+                }
+            });
+            </script>
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
+
+md_text = open(os.path.join(filepath, "01_free_particle.md"), "r").read()
+
+
+def getfig2(k=4, dk=2, xmax=5):
+    # Defining functions
+    def psi_contour(x, dk):
+        return (np.sin(dk*x)/(np.sqrt(np.pi*dk)*x))
+
+    def psi(x, k, dk):
+        return psi_contour(x, dk)*(np.cos(k*x)+np.sin(k*x)*1j)
+
+    # calculation (Prepare data)
+    x = np.linspace(-xmax, xmax, 900)
+    y_real = psi(x, k, dk).real
+    y_imag = psi(x, k, dk).imag
+    y_prob = (psi(x, k, dk).real)**2+(psi(x, k, dk).imag)**2
+
+    # Plot
+    title1 = r'$ \text {Real contribution to } \Psi_{\Delta k}(x)$'
+    title2 = r'$ \text {Imaginary contribution to } \Psi_{\Delta k}(x)$'
+    title3 = r'$ \text {Probability Density }$'
+    fig = make_subplots(
+        rows=2, cols=2, subplot_titles=(title1, title2, title3))
+
+    # 1st subplot
+    fig.append_trace(go.Scatter(x=x, y=y_real, name="Real",
+                                line=dict(color='blue')), row=1, col=1)
+    fig.append_trace(go.Scatter(x=x, y=psi_contour(x, dk), showlegend=False, line=dict(
+        color='blue', width=1, dash='dash')), row=1, col=1)
+    fig.append_trace(go.Scatter(x=x, y=-psi_contour(x, dk), showlegend=False,
+                                line=dict(color='blue', width=1, dash='dash')), row=1, col=1)
+    fig.update_xaxes(title_text=r"$x (Å)$", row=1, col=1)
+    fig.update_yaxes(title_text=r'$\Psi_{\Delta k}(x)$', row=1, col=1)
+
+    # 2nd subplot
+    fig.append_trace(go.Scatter(x=x, y=y_imag, name="Imag",
+                                line=dict(color='red')), row=1, col=2, )
+    fig.append_trace(go.Scatter(x=x, y=psi_contour(x, dk), showlegend=False, line=dict(
+        color='red', width=1, dash='dash')), row=1, col=2)
+    fig.append_trace(go.Scatter(x=x, y=-psi_contour(x, dk), showlegend=False,
+                                line=dict(color='red', width=1, dash='dash')), row=1, col=2)
+    fig.update_xaxes(title_text=r"$x (Å)$", row=1, col=2)
+    fig.update_yaxes(title_text=r'$\Psi_{\Delta k}(x)$', row=1, col=2)
+
+    # 3rd subplot
+    fig.append_trace(go.Scatter(
+        x=x, y=y_prob, name="Probability Density", line=dict(color='green')), row=2, col=1)
+    fig.update_xaxes(title_text=r"$x (Å)$", row=2, col=1)
+    fig.update_yaxes(
+        title_text=r'$\left|\Psi_{\Delta k}(x)\right|^2$', row=2, col=1)
+
+    # show
+    title = r"$k_o  \pm \Delta k = {} \pm {} Å^{{-1}}$".format(k, dk)
+    fig.update_layout(height=600, title_text=title)
+    return fig
+
+###### important for latex ######
+axis_latex_script = dji.Import(src="https://codepen.io/yueyericardo/pen/pojyvgZ.js")
+mathjax_script = dji.Import(src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/latest.js?config=TeX-AMS-MML_SVG")
+
+# fig2
+fig2 = dcc.Graph(figure=getfig2(), id="fig2")
+sliders2 = html.Div([
+    html.Label('The value for $k_0$ (in $ Å^{-\;1} $)'),
+    dcc.Slider(id='fig2_k_slider', min=1, max=15, value=4, marks={str(x): str(x) for x in np.arange(1, 16, 1)}, step=1),
+    html.Label('The value for $\Delta k$ (in $ Å^{-\;1} $)'),
+    dcc.Slider(id='fig2_dk_slider', min=1, max=15, value=2, marks={str(x): str(x) for x in np.arange(1, 16, 1)}, step=1),
+    html.Label('the maximum value for x (in Å)'),
+    dcc.Slider(id='fig2_xmax_slider', min=1, max=15, value=5, marks={str(x): str(x) for x in np.arange(1, 16, 1)}, step=1),
+    ], style={'columnCount': 3, 'padding': '0'})
+
+
+app.layout = html.Div([
+    dcc.Markdown(md_text, dangerously_allow_html=True),
+    fig2, sliders2,
+    ###### important for latex ######
+    axis_latex_script,
+    ###### important for latex ######
+    mathjax_script,
+])
+
+
+# update_fig2
+@app.callback(
+    Output('fig2', 'figure'),
+    [Input('fig2_k_slider', 'value'),
+     Input('fig2_dk_slider', 'value'),
+     Input('fig2_xmax_slider', 'value'),
+     ])
+def update_fig2(k, dk, xmax):
+    fig = getfig2(k=k, dk=dk, xmax=xmax)
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', debug=True)
